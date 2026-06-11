cd1abfbf715de4a7428bae9f838ef38ac8746e56
Hyeonu Lee <leehyeonu0801@gmail.com>
2026-06-12 01:59:03 +0900

    manifest

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -42,7 +42,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/apple-touch-icon.png",
